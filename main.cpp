f3c7608ea615d653e8afdcc102bcf389a4539ac0
Seth Weiland <42858933+sethweiland@users.noreply.github.com>
2019-05-15 17:59:15 -0700

    sketched out a possible main
    note: here makeAttack(Move, string), have changed the function locally to accept two parameters but havent put in the master yet.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,47 @@
-int main(){}
+#include "Pokemon.cpp"
+using namespace std;
+int main() {
+    Pokemon p1;
+    Pokemon p2;
+    while(1) {
+        
+        for(int i=1;i<3;i++){
+            printf ("Player %d, it is your turn, choose an attack: \n", i);
+            if(i==1) {
+                int atkChoice;
+                cout<<"1. "<<p1.movelist[0].atkName<<"\n";
+                cout<<"2. "<<p1.movelist[1].atkName<<"\n";
+                cout<<"3. "<<p1.movelist[2].atkName<<"\n";
+                cout<<"4. "<<p1.movelist[3].atkName<<"\n";
+                cin>>atkChoice;
+                double dmg1=p2.MakeAttack(p2.movelist[atkChoice], p1.getType());
+                p1.TakeDamage(dmg1);
+                cout<<"\nPlayer 1 Health: "<<p1.hp<<"\n";
+                cout<<"Player 2 Health: "<<p2.hp<<"\n";
+                
+            }
+            if(i==2) {
+                int atkChoice;
+                cout<<"1. "<<p2.movelist[0].atkName<<"\n";
+                cout<<"2. "<<p2.movelist[1].atkName<<"\n";
+                cout<<"3. "<<p2.movelist[2].atkName<<"\n";
+                cout<<"4. "<<p2.movelist[3].atkName<<"\n";
+                cin>>atkChoice;
+                double dmg2=p2.MakeAttack(p2.movelist[atkChoice], p1.getType());
+                p1.TakeDamage(dmg2);
+                cout<<"\nPlayer 1 Health: "<<p1.hp<<"\n";
+                cout<<"Player 2 Health: "<<p2.hp<<"\n";
+            }
+        }
+        if(p1.hp<=0 | p2.hp<=0) {
+            break;
+        }
+    }
+    if(p1.hp<=0) {
+        cout<<"Player 2 is the winner\n";
+    }
+    else {
+        cout<<"Player 1 is the winner\n";
+    }
+}
+
