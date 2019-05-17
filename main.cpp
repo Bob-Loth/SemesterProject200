1798fccc2e3dfa247221cbdf23e6057e61ef5068
Seth Weiland <42858933+sethweiland@users.noreply.github.com>
2019-05-17 08:53:01 -0700

    update with speed factored into who can move first

--- main.cpp
+++ main.cpp
@@ -1,52 +1,89 @@
 
+
+#include <iostream>
+//#include "Pokemon.cpp"
 #include "Pokemon.h"
 using namespace std;
 int main() {
     Pokemon p1;
     Pokemon p2;
  
+if(p1.spd>=p2.spd) {
+    
     while(1) {
-        for(int i=1;i<3;i++){
-            printf ("Player %d, it is your turn, choose an attack: \n", i);
-            if(i==1) {
-                int atkChoice;
+            //printf ("Player %d, it is your turn, choose an attack: \n", i);
+                cout<<"Player 1 it is your move, choose an attack: \n";
+                int atkChoice1;
+                int atkChoice2;
                 cout<<"1. "<<p1.movelist[0].atkName<<"\n";
                 cout<<"2. "<<p1.movelist[1].atkName<<"\n";
                 cout<<"3. "<<p1.movelist[2].atkName<<"\n";
                 cout<<"4. "<<p1.movelist[3].atkName<<"\n";
-                cin>>atkChoice;
-                string opp_type = p2.getType();
-                double dmg1=p1.MakeAttack(p2.movelist[atkChoice-1], opp_type);
+                cin>>atkChoice1;
+                string opp_type2 = p2.getType();
+                double dmg1=p1.MakeAttack(p1.movelist[atkChoice1-1], opp_type2);
                 p2.TakeDamage(dmg1);
-                cout<<"\nPlayer 1 Health: "<<p1.hp<<"\n";
-                cout<<"Player 2 Health: "<<p2.hp<<"\n\n";
-                if(p2.hp<0) {break;}  
-            }
-            if(i==2) {
-                int atkChoice;
+                cout<<"\nPlayer 1 Health: "<<max(p1.hp,0)<<"\n";
+                cout<<"Player 2 Health: "<<max(p2.hp,0)<<"\n\n";
+                if(p2.hp<=0) {break;}
+        
+                cout<<"Player 2 it is your move, choose an attack: \n";
                 cout<<"1. "<<p2.movelist[0].atkName<<"\n";
                 cout<<"2. "<<p2.movelist[1].atkName<<"\n";
                 cout<<"3. "<<p2.movelist[2].atkName<<"\n";
                 cout<<"4. "<<p2.movelist[3].atkName<<"\n";
-                cin>>atkChoice;
-                string opp_type=p1.getType();
-                double dmg2=p2.MakeAttack(p2.movelist[atkChoice-1], opp_type);
-                
+                cin>>atkChoice2;
+                string opp_type1=p1.getType();
+                double dmg2=p2.MakeAttack(p2.movelist[atkChoice2-1], opp_type1);
                 p1.TakeDamage(dmg2);
-                cout<<"\nPlayer 1 Health: "<<p1.hp<<"\n";
-                cout<<"Player 2 Health: "<<p2.hp<<"\n\n";
-            }
-        }
-        if(p1.hp<=0 | p2.hp<=0) {
-            break;
-        }
+                cout<<"\nPlayer 1 Health: "<<max(p1.hp,0)<<"\n";
+                cout<<"Player 2 Health: "<<max(p2.hp,0)<<"\n\n";
+                if(p1.hp<=0) {
+                    break;
+                }
     }
-    if(p1.hp<=0) {
-        cout<<"Player 2 is the winner\n";
-    }
-    else {
-        cout<<"Player 1 is the winner\n";
-    }
+    if(p2.hp<=0) {cout<<"Player 1 is the winner\n"; }
+    if(p1.hp<=0) {cout<<"Player 2 is the winner\n"; }
+    
 }
 
+else if (p2.spd>p1.spd) {
+        
+        while(1) {
+            //printf ("Player %d, it is your turn, choose an attack: \n", i);
+            
+            cout<<"Player 2 it is your move, choose an attack: \n";
+            int atkChoice1;
+            int atkChoice2;
+            cout<<"1. "<<p2.movelist[0].atkName<<"\n";
+            cout<<"2. "<<p2.movelist[1].atkName<<"\n";
+            cout<<"3. "<<p2.movelist[2].atkName<<"\n";
+            cout<<"4. "<<p2.movelist[3].atkName<<"\n";
+            cin>>atkChoice2;
+            string opp_type1 = p1.getType();
+            double dmg2=p2.MakeAttack(p2.movelist[atkChoice2-1], opp_type1);
+            p2.TakeDamage(dmg2);
+            cout<<"\nPlayer 1 Health: "<<max(p1.hp,0)<<"\n";
+            cout<<"Player 2 Health: "<<max(p2.hp,0)<<"\n\n";
+            if(p1.hp<0) {break;}
+            
+            cout<<"Player 1 it is your move, choose an attack: \n";
+            cout<<"1. "<<p1.movelist[0].atkName<<"\n";
+            cout<<"2. "<<p1.movelist[1].atkName<<"\n";
+            cout<<"3. "<<p1.movelist[2].atkName<<"\n";
+            cout<<"4. "<<p1.movelist[3].atkName<<"\n";
+            cin>>atkChoice1;
+            string opp_type2=p2.getType();
+            double dmg1=p1.MakeAttack(p1.movelist[atkChoice1-1], opp_type2);
+            p2.TakeDamage(dmg1);
+            cout<<"\nPlayer 1 Health: "<<max(p1.hp,0)<<"\n";
+            cout<<"Player 2 Health: "<<max(p2.hp,0)<<"\n\n";
+            if(p2.hp<=0) {break;}
+            }
+        
+    if(p2.hp<=0) {cout<<"Player 1 is the winner\n"; }
+    if(p1.hp<=0) {cout<<"Player 2 is the winner\n"; }
+}
+        
+}
 
